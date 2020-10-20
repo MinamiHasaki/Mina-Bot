fa5f5ab850f6a3c321f112a124931c22d9e60a40
Minami Hasaki <MinamiHasaki620@Gmail.com>
2020-10-20 18:13:33 -0400

    Updated AmongUs Cog

--- Mina-Bot.py
+++ Mina-Bot.py
@@ -8,20 +8,23 @@ client = commands.Bot(command_prefix="!", case_insensitive=True)
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f"cogs.{extension.lower()}")
-    await ctx.send(f"`{extension.lower()} loaded!`")
+    await ctx.send(f"`{extension} loaded!`", delete_after=10)
+    await ctx.message.delete(delay=10)
 
 
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f"cogs.{extension.lower()}")
-    await ctx.send(f"`{extension.lower()} unloaded!`")
+    await ctx.send(f"`{extension} unloaded!`", delete_after=10)
+    await ctx.message.delete(delay=10)
 
 
 @client.command()
 async def restart(ctx, extension):
     client.unload_extension(f"cogs.{extension.lower()}")
     client.load_extension(f"cogs.{extension.lower()}")
-    await ctx.send(f"`{extension.lower()} restarted!`")
+    await ctx.send(f"`{extension} restarted!`", delete_after=10)
+    await ctx.message.delete(delay=10)
 
 
 @client.command()
@@ -30,8 +33,9 @@ async def reload(ctx):
         if file.endswith(".py"):
             client.unload_extension(f"cogs.{file[:-3]}")
             client.load_extension(f"cogs.{file[:-3]}")
-            await ctx.send(f"`{file[:-3]} loaded!`")
-    await ctx.send("`All cogs reloaded!`")
+            await ctx.send(f"`{file[:-3]} loaded!`", delete_after=10)
+    await ctx.send("`All cogs reloaded!`", delete_after=10)
+    await ctx.message.delete(delay=10)
 
 
 def load_all_cogs():
--- cogs/amongus.py
+++ cogs/amongus.py
@@ -11,41 +11,35 @@ class AmongUs(commands.Cog):
     # Among Us Party Invite Command
     @commands.command()
     async def invite(self, ctx, code, region):
+        await ctx.message.delete()
         if hasattr(ctx.author.voice, 'channel'):
-            code = code.upper()
-            region = region.upper()
-            author = ctx.author
-            author_avatar = ctx.author.avatar_url
-            room_link = await ctx.author.voice.channel.create_invite(max_age=1800)
-            room_name = ctx.author.voice.channel
-            room_size = len(ctx.author.voice.channel.members)
-            room_limit = ctx.author.voice.channel.user_limit
             embed = discord.Embed(
                 colour=discord.Colour(0x2DC7FF),
-                description=f"[:arrow_right: **Click to join!** :arrow_left:]({room_link})\nIf you want to make your "
+                description=f"[:arrow_right: **Click to join!** :arrow_left:]"
+                            f"({await ctx.author.voice.channel.create_invite(max_age=1800)})\nIf you want to make your "
                             f"own party, join a voice channel and type `!invite` right here. ")
-            embed.set_author(name=f"{author} is looking for party members!",
-                             icon_url=f"{author_avatar}")
+            embed.set_author(name=f"{ctx.author} is looking for party members!",
+                             icon_url=f"{ctx.author.avatar_url}")
             embed.set_thumbnail(url="https://puu.sh/GBPrB/0aee3ee079.png")
             embed.add_field(name="**__Channel__**",
-                            value=f"**{room_name}**",
+                            value=f"**{ctx.author.voice.channel}**",
                             inline=True)
             embed.add_field(name="**__Party Size__**",
-                            value=f"**{room_size} / {room_limit}**",
+                            value=f"**{len(ctx.author.voice.channel.members)} "
+                                  f"/ {ctx.author.voice.channel.user_limit}**",
                             inline=True)
-            embed.add_field(name="__Room Code & Region__**", value=f"**{code} || {region}**", inline=True)
+            embed.add_field(name="__Room Code & Region__**", value=f"**{code.upper()} "
+                                                                   f"|| {region.upper()}**", inline=True)
             embed.set_footer(text="Post your party here. Simply do !invite or post an invite link. (INV)")
-            await ctx.message.delete()
             await ctx.send(embed=embed)
         else:
-            await ctx.message.delete()
-            await ctx.send("Please join a voice channel first!", delete_after=5)
+            await ctx.send(f"{ctx.author.mention}, please join a voice channel first!", delete_after=10)
 
 
 # Called To Load Cog And Connect To Client
 def setup(client):
     client.add_cog(AmongUs(client))
-    print("AmongUs unloaded!")
+    print("AmongUs loaded!")
 
 
 # Called When Cog Is Unloaded
